623d342d9f8f6ca6faa4e44232c5b231df27c531
Jongwon Park <pjw970220@gmail.com>
2024-08-16 03:21:33 +0900

    [edit] 디스플레이 크기에 따른 표현 수정
    화면 크기에 따라 부자연스럽게 보이던 부분 수정

--- components/line/DashedLine.tsx
+++ components/line/DashedLine.tsx
@@ -1,10 +1,11 @@
+import responsive from "@/tools/ratio";
 import React from "react";
 import { View } from "react-native";
 
 interface Props {
-  dash?: number;
-  gap?: number;
-  dashLength?: number;
+  dash: number;
+  gap: number;
+  dashLength: number;
   color?: string;
 }
 
@@ -13,17 +14,22 @@ function DashedLine(props: Props) {
     <View
       style={{
         height: 1,
-        width: dash ?? 4,
+        width: responsive(dash) ?? 4,
         backgroundColor: props?.color ?? "#333333",
         gap: gap ?? 4,
       }}
     />
   );
 
-  const line = Array(props?.dashLength)
+  const line = Array(props.dashLength)
     .fill(null)
     .map((_, index) => (
-      <Line key={index} dash={props?.dash} gap={props?.gap} />
+      <Line
+        key={index}
+        dash={props?.dash}
+        gap={props?.gap}
+        dashLength={props.dashLength}
+      />
     ));
 
   return (
@@ -31,7 +37,6 @@ function DashedLine(props: Props) {
       style={{
         flexDirection: "row",
         justifyContent: "space-between",
-        // alignSelf: "center",
       }}
     >
       {line}
